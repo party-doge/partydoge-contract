c2062b5e5e53f89cf4d4f9e02d322189e48dad11
jasonGhostDev <hyotiger777@gmail.com>
2021-09-04 10:50:58 +1000

    deploy script with truffle

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -7,23 +7,6 @@ const configs = require("../config.json");
 
 module.exports = async function(deployer) {
   try {
-    let dataParse = {};
-
-    if (!configs.PartyDoge) {
-      await deployer.deploy(PartyDoge, {
-        gas: 4000000
-      });
-    
-      dataParse['PartyDoge'] = PartyDoge.address;
-      
-    }
-    else {
-      dataParse['PartyDoge'] = configs.PartyDoge;
-    }
-  
-
-    const updatedData = JSON.stringify(dataParse);
-		await fs.promises.writeFile('contracts.json', updatedData);
 
   } catch (error) {
     console.log(error);
