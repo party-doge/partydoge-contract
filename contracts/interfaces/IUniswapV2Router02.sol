2075e211a18375d6c529df75af78d55334c91983
jasonGhostDev <hyotiger777@gmail.com>
2021-09-05 03:14:43 +1000

    change contract size

--- contracts/interfaces/IUniswapV2Router02.sol
+++ contracts/interfaces/IUniswapV2Router02.sol
@@ -1,7 +1,7 @@
 
 // SPDX-License-Identifier: MIT
 import "./IUniswapV2Router01.sol";
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 interface IUniswapV2Router02 is IUniswapV2Router01 {
     function removeLiquidityETHSupportingFeeOnTransferTokens(
